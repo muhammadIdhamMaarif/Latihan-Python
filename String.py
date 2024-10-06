8907dbb89c8a7c2758ecea017fe5168bc3c21d8d
muhammadIdhamMaarif <muhammadidhammaarif@gmail.com>
2024-10-06 20:10:00 +0700

    Create String.py

---
+++ String.py
@@ -0,0 +1,6 @@
+kalimat = "Belajar Python sangat menyenangkan"
+print(kalimat[:5])
+print(kalimat.lower())
+kalimat_baru = kalimat.replace("Python", "Pemrograman") 
+print(kalimat_baru)
+print(kalimat.count('a'))
