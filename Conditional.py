bc825715a0645c2bcd708b4ca5d29365bebf0926
muhammadIdhamMaarif <muhammadidhammaarif@gmail.com>
2024-10-06 20:10:25 +0700

    Create Conditional.py

---
+++ Conditional.py
@@ -0,0 +1,10 @@
+nilai = int(input("Masukkan nilai ujian: "))
+if nilai >= 90:
+    print("Sangat Baik")
+elif 80 <= nilai < 90:
+    print("Baik Sekali")
+elif 70 <= nilai < 80:
+    print("Baik")
+else:
+    print("Perlu Belajar Lebih Giat")
+
